a764778a3a486bae9f0e9141c3b963a7905c4da0
dani-sam <samdani295@gmail.com>
2021-12-16 16:27:31 +0530

    rowPost&style API Integration

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from './components/NavBar/NavBar';
 import './App.css';
+import {action,originals,trending,comedy,horror,romance,documentaries} from './urls'
 import Banner from './components/Banner/Banner';
 import RowPost from './components/RowPost/RowPost';
 
@@ -9,7 +10,13 @@ function App() {
     <div className="App">
       <NavBar/>
       <Banner/>
-      <RowPost/>
+      <RowPost url={originals} title='Netflix Original' />
+      <RowPost url={action} title='Action' isSmall />
+      <RowPost url={trending} title='Trending' isSmall />
+      <RowPost url={comedy} title='Comedy' isSmall />
+      <RowPost url={horror} title='Horror' isSmall />
+      <RowPost url={romance} title='Romance' isSmall />
+      <RowPost url={documentaries} title='Documentaries' isSmall />
     </div>
   );
 }
